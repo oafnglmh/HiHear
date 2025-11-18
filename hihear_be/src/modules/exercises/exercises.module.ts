fcfff9c684f1116ef81b18cc95fd23ef39c07530
ThuongHoai081 <th316942@gmail.com>
2025-11-18 18:50:17 +0700

    feat(admin): fix module exercise

--- hihear_be/src/modules/exercises/exercises.module.ts
+++ hihear_be/src/modules/exercises/exercises.module.ts
@@ -5,12 +5,16 @@ import { ExerciseService } from './exercises.service';
 import { ExerciseController } from './exercises.controller';
 import { ExerciseVocabularyModule } from '../exercise-vocabulary/exercise-vocabulary.module';
 import { ExerciseGrammaModule } from '../exercise-gramma/exercise-gramma.module';
+import { ExerciseListeningModule } from '../exercise-listening/exercise-listening.module';
+import { MediaModule } from '../media/media.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([ExercisesEntity]),
     ExerciseVocabularyModule,
     ExerciseGrammaModule,
+    ExerciseListeningModule,
+    MediaModule,
   ],
   controllers: [ExerciseController],
   providers: [ExerciseService],
